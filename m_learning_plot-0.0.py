5d9450027daa938a60db9a69bb13c607d4a326b1
ErmannoFriso <e.friso@euromag.com>
2023-08-15 14:30:02 +0200

    merge

---
+++ m_learning_plot-0.0.py
@@ -0,0 +1,21 @@
+# summarize the data
+from pandas import read_csv
+from matplotlib import pyplot
+from pandas.plotting import scatter_matrix
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+# box and whisker plots
+# dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+# pyplot.show()
+
+# histograms
+# dataset.hist()
+# pyplot.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
